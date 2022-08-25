43afaa0f49e6e13c13b132b34b22cbca1f78aa25
leorumyantsev <rumiantsev.l@gmail.com>
2022-08-25 15:43:57 +0300

    Initial commit

---
+++ AreaCalculator/GeometricObject.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AreaCalculator
+{
+    public abstract class GeometricObject
+    {
+        public abstract double GetArea();
+    }
+}
---
+++ AreaCalculator/Triangle.cs
@@ -0,0 +1,7 @@
+namespace AreaCalculator
+{
+    public class Triangle
+    {
+        
+    }
+}
