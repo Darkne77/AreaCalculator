61dfeb3d20ca55d94182f176b69d58e83204ffe8
leorumyantsev <rumiantsev.l@gmail.com>
2022-08-25 15:45:14 +0300

    Add project file

--- AreaCalculator/Circle.cs
+++ AreaCalculator/Circle.cs
@@ -1,7 +1,19 @@
+using System;
+
 namespace AreaCalculator
 {
-    public class Circle
+    public class Circle : GeometricObject
     {
+        public double Radius { get; }
         
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
     }
 }
--- AreaCalculator/Triangle.cs
+++ AreaCalculator/Triangle.cs
@@ -1,7 +1,25 @@
+using System;
+
 namespace AreaCalculator
 {
-    public class Triangle
+    public class Triangle : GeometricObject
     {
-        
+        public int A { get; }
+        public int B { get; }
+        public int C { get; }
+
+        public double S => 1 / 2 * (A + B + C);
+
+        public Triangle(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public override double GetArea()
+        {
+            return Math.Sqrt(S * (S - A) * (S - B) * (S - C));
+        }
     }
 }
